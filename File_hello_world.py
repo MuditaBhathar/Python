4ca813c34509c970c66e5bcd0fef74daca8c9515
Mudita Bhathar <120955114+MuditaBhathar@users.noreply.github.com>
2022-12-20 19:42:25 +0530

    Create File_hello_world.py

---
+++ File_hello_world.py
@@ -0,0 +1,4 @@
+#8.using file handling – write a text file in ur system with “hello world”.
+file=open('Text2.txt','w') #already text file saved with name Text2
+file.write('hello world')
+file.close()
