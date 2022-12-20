d7b94528d8ba027428cd5655e2ca2d1cc31c017e
Mudita Bhathar <120955114+MuditaBhathar@users.noreply.github.com>
2022-12-20 17:48:44 +0530

    Create Two_list.py

---
+++ Two_list.py
@@ -0,0 +1,5 @@
+#urlist = ['wood','knife','axe'] , mylist = ['tree', 'apple', 'mango', 'melon'] – combine two lists
+urlist=['wood','knife','axe']
+mylist=['tree','apple','mango','melon']
+ourlist=urlist+mylist
+print(ourlist)
